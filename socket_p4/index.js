0af72da4cb1452eeccb376b174cab954b5ed2de7
TangCo <17520296@gm.uit.edu.vn>
2021-05-03 19:11:04 +0700

    pussh code

--- socket_p4/index.js
+++ socket_p4/index.js
@@ -25,27 +25,60 @@ const interfaceUser = (id, name, pass) => {
         password: pass
     };
 };
-
+const arrRoomInitial = [];
 io.on('connection', function (socket) {
-    console.log('User connect to server: ', socket.id);
-    console.log('room: ', socket.adapter.rooms);
+    // console.log('User connect to server: ', socket.id);
+    // console.log('room: ', socket.adapter.rooms);
+    arrRoomInitial.push(socket.id);
     socket.on('disconnect', function () {
+        for (let i = 0; i < arrRoomInitial.length; i++) {
+            if (arrRoomInitial[i] === socket.id) {
+                arrRoomInitial.splice(i, 1);
+                break;
+            }
+        }
         console.log(socket.id + ' has disconnected to server');
     });
     socket.on('tao-room', function(data) {
         socket.join(data);
+        if (socket.Phong) {
+            socket.leave(socket.Phong);
+            delete socket.Phong;
+        }
         socket.Phong = data;
         const listRoom = socket.adapter.rooms;
         const MangRoom= [];
         listRoom.forEach(function(person, tenPhong) {
-            MangRoom.push(tenPhong);
+            let tenPhongInitial;
+            person.forEach(function(item) {
+                tenPhongInitial = item;
+            });
+            if (!arrRoomInitial.includes(tenPhong)) {
+                MangRoom.push(tenPhong);
+            }
         });
         io.sockets.emit('server-send-rooms', MangRoom);
         socket.emit('server-send-room-socket', data);
+        console.log('room: ', socket.adapter.rooms);
     });
     socket.on('user-chat', function(data){
         // Chat trong 1 phong
         io.sockets.in(socket.Phong).emit('server-chat', data);
+    });
+    socket.on('chuyen-room', function(data) {
+        if (socket.Phong) {
+            socket.leave(socket.Phong);
+            delete socket.Phong;
+        }
+        if (data.targetRoom) {
+            socket.join(data.targetRoom);
+            socket.Phong = data.targetRoom;
+        }
+        if (data.currentRoom) {
+            socket.leave(data.currentRoom);
+        }
+        socket.emit('server-send-room-socket', data.targetRoom);
+        console.log('chuyen room: ', socket.adapter.rooms);
     });
 });
 
--- socket_p4/public/process.js
+++ socket_p4/public/process.js
@@ -2,28 +2,45 @@ const socket = io("http://localhost:3000");
 
 $(document).ready(function () {
     // Nhan danh sach rooms tu server
-    socket.on('server-send-rooms', function(data) {
+    socket.on('server-send-rooms', function (data) {
         $('#dsRoom').html("");
-        data.map(function(item) {
-            $('#dsRoom').append("<h4 class='room' >" + item + "</h4>");
-        })
+        data.map(function (item) {
+            $('#dsRoom').append("<h4 class='room' id = '" + item + "' >" + item + "</h4>");
+        });
+        const btnContainer = document.getElementById('dsRoom');
+        const btns = btnContainer.getElementsByClassName("room");
+        // current room
+        const currentRoom = $('#txtRoom').val();
+        for (let i = 0; i < btns.length; i++) {
+            btns[i].addEventListener("click", function () {
+                socket.emit('chuyen-room', {
+                    targetRoom: data[i],
+                    currentRoom: currentRoom
+                });
+            });
+        }
     });
 
 
-    $('#btnTaoRoom').click(function() {
+    $('#btnTaoRoom').click(function () {
         socket.emit('tao-room', $('#txtRoom').val());
     });
 
-    socket.on('server-send-room-socket', function(data) {
+    socket.on('server-send-room-socket', function (data) {
         $('#roomHienTai').html(data);
     });
 
-    $('#bthChat').click(function() {
+    $('#bthChat').click(function () {
         socket.emit('user-chat', $('#txtMessenger').val());
         $('#txtMessenger').val('');
     });
 
-    socket.on('server-chat', function(data) {
-        $('#right').append('<div>'+ data +'</div>');
+    socket.on('server-chat', function (data) {
+        $('#right').append('<div>' + data + '</div>');
     });
+    $('.room').click(function () {
+        const x = document.getElementsByClassName('room');
+        console.log(x);
+    });
+
 });
